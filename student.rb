5c4d031361ae802b883e02a0524af9d5982d7884
Linda <ltramos7@gmail.com>
2020-10-19 09:13:40 -0700

    added the attribute to attr_reader and deleted the getter method for major.

--- student.rb
+++ student.rb
@@ -1,14 +1,9 @@
 class Student
-    attr_reader :name
+    attr_reader :name, :major
 
     def initialize(name, major)
         @name = name
         @major = major
-    end
-
-    # A getter method for major.
-    def major
-        @major
     end
 
     # A setter method for major.
